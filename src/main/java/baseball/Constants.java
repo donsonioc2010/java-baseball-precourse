d8b6da11ec222a1911b2e7bfe03fa0ce6b541ebe
donsonioc2010 <whddnjs822@gmail.com>
2021-10-03 15:29:15 +0900

    feat(Constants) : 상수값 문자열 수정

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String GET_NUM_STR      = "숫자를 입력해주세요 : ";
     public static final String PRT_STRIKE       = "스트라이크";
     public static final String PRT_BALL         = "볼";
-    public static final String PRT_REGAME       = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+    public static final String PRT_REGAME       = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ";
     public static final String PRT_ENDGAME      = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
     public static final String PRT_NOTHING      = "낫싱";
     public static final String PRT_ERROR        = "[ERROR]";
