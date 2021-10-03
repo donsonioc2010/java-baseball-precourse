de09795235c31aba587b471156ea71173876a2ef
donsonioc2010 <whddnjs822@gmail.com>
2021-10-03 22:54:31 +0900

    feat(ModeAdmin) : For문에서 사용중이던 변수 전역변수로 변경

--- src/main/java/baseball/ModeAdmin.java
+++ src/main/java/baseball/ModeAdmin.java
@@ -4,16 +4,16 @@ package baseball;
 import nextstep.utils.Console;
 
 public class ModeAdmin {
+    private boolean getInValConfirm = true;
     public ModeAdmin() {
     }
     /* 게임의 시작과 종료의 결과를 알려줄 메소드 게임의 시작 */
     public boolean getRestartVal() {
-        boolean forFirstWhile = false;
         String runMode = null;
-        while(!forFirstWhile){
+        while(getInValConfirm){
             System.out.print(Constants.PRT_REGAME);
             runMode = Console.readLine().trim();
-            forFirstWhile = validationModeVal(runMode);
+            validationModeVal(runMode);
         }
         System.out.println();
         return confirmValue(runMode);
@@ -21,20 +21,18 @@ public class ModeAdmin {
 
     /* 정상적인 값을 받으면 어떤값인지 리턴함  값이 1인경우 true그래야 게임이 재시작함. 해당 메소드는 값이 무조건 정상인 경우만 실행함. */
     private boolean confirmValue(String runMode) {
-        if(Constants.REGAME_VAL.equals(runMode)) {
-            return true;
-        }
-        return false;
+        return Constants.REGAME_VAL.equals(runMode);
     }
 
 
     /* 받은 문자열이 정상인지 확인 후 오류시 메세지 출력, 1 또는 2인 경우에만 true 해야함 */
-    private boolean validationModeVal(String runMode) {
+    private void validationModeVal(String runMode) {
         if(Constants.REGAME_VAL.equals(runMode) || Constants.ENDGAME_VAL.equals(runMode)) {
-            return true;
+            getInValConfirm = false;
+            return;
         }
         System.out.println(Constants.PRT_ERROR);
-        return false;
+        getInValConfirm = true;
     }
 
 }
